92334d382d359533faf1cfcc25a5575bf0edb022
Jesse Towner <townerj@gmail.com>
2017-10-09 10:24:24 -0700

    - added utf8::enable_if_char_input_iterator_t and restrict overload resolution on utf8 functions using it - renamed a few variables in utf8::decode_rune_octet to be more clear

--- lug/utf8.hpp
+++ lug/utf8.hpp
@@ -14,9 +14,15 @@
 #include <algorithm>
 #include <iterator>
 #include <utility>
+#include <type_traits>
 
 namespace lug::utf8
 {
+
+template <class InputIt, class T = void>
+using enable_if_char_input_iterator_t = std::enable_if_t<!std::is_integral_v<InputIt> &&
+	std::is_base_of_v<std::input_iterator_tag, typename std::iterator_traits<InputIt>::iterator_category> &&
+	std::is_same_v<char, std::remove_cv_t<typename std::iterator_traits<InputIt>::value_type>>, T>;
 
 constexpr unsigned int decode_accept = 0;
 constexpr unsigned int decode_reject = 12;
@@ -28,7 +34,7 @@ constexpr bool is_lead(char octet) noexcept
 
 inline unsigned int decode_rune_octet(char32_t& rune, char octet, unsigned int state)
 {
-	static constexpr std::array<unsigned char, 256> dfa_class
+	static constexpr std::array<unsigned char, 256> dfa_class_table
 	{
 		 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 		 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -48,7 +54,7 @@ inline unsigned int decode_rune_octet(char32_t& rune, char octet, unsigned int s
 		11, 6, 6, 6, 5, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8
 	};
 
-	static constexpr std::array<unsigned char, 108> dfa_transition
+	static constexpr std::array<unsigned char, 108> dfa_transition_table
 	{
 		 0,12,24,36,60,96,84,12,12,12,48,72,12,12,12,12,
 		12,12,12,12,12,12,12,12,12, 0,12,12,12,12,12, 0,
@@ -59,38 +65,36 @@ inline unsigned int decode_rune_octet(char32_t& rune, char octet, unsigned int s
 		12,36,12,12,12,12,12,12,12,12,12,12
 	};
 
-	const unsigned int uo = static_cast<unsigned char>(octet);
-	const unsigned int cc = dfa_class[uo];
-	rune = state == decode_accept ? uo & (0xff >> cc) : (uo & 0x3f) | (rune << 6);
-	return dfa_transition[state + cc];
+	unsigned int const symbol = static_cast<unsigned char>(octet);
+	unsigned int const dfa_class = dfa_class_table[symbol];
+	rune = state == decode_accept ? symbol & (0xff >> dfa_class) : (symbol & 0x3f) | (rune << 6);
+	return dfa_transition_table[state + dfa_class];
 }
 
-template <class InputIt>
+template <class InputIt, class = enable_if_char_input_iterator_t<InputIt>>
 inline std::pair<char32_t, InputIt> decode_rune(InputIt first, InputIt last)
 {
 	char32_t rune = U'\0';
 	unsigned int state = decode_accept;
-	while (first != last && state != decode_reject) {
-		state = ::lug::utf8::decode_rune_octet(rune, *first++, state);
-		if (state == decode_accept)
+	while (first != last && state != decode_reject)
+		if (state = ::lug::utf8::decode_rune_octet(rune, *first++, state); state == decode_accept)
 			return ::std::make_pair(rune, first);
-	}
 	return ::std::make_pair(U'\U0000fffd', ::std::find_if(first, last, ::lug::utf8::is_lead));
 }
 
-template <class InputIt>
+template <class InputIt, class = enable_if_char_input_iterator_t<InputIt>>
 inline InputIt next_rune(InputIt first, InputIt last)
 {
 	return ::lug::utf8::decode_rune(first, last).second;
 }
 
-template <class InputIt>
+template <class InputIt, class = enable_if_char_input_iterator_t<InputIt>>
 inline std::size_t size_of_first_rune(InputIt first, InputIt last)
 {
 	return static_cast<std::size_t>(::std::distance(first, ::lug::utf8::next_rune(first, last)));
 }
 
-template <class InputIt>
+template <class InputIt, class = enable_if_char_input_iterator_t<InputIt>>
 inline std::size_t count_runes(InputIt first, InputIt last)
 {
 	std::size_t count = 0;
