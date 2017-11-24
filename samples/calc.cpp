a688db4edf240770facf60ee207ca2b78b3d8704
Jesse Towner <townerj@gmail.com>
2017-11-24 01:18:19 -0800

    compact rules

--- samples/calc.cpp
+++ samples/calc.cpp
@@ -15,38 +15,32 @@ namespace samples::calc
 	variable<int> i{Sema};
 	double v[26];
 
+	extern rule Expr;
+
 	implicit_space_rule BLANK = lexeme[ *"[ \t]"s ];
 
 	rule EOL	= lexeme[ "[\n\r;]" ];
-
 	rule ID		= lexeme[ capture(m)[ "[a-z]" ] <[]() -> int { return m->at(0) - 'a'; } ];
-
 	rule NUMBER = lexeme[ capture(m)[ ~"[-+]"s > +"[0-9]"s > ~("[.]" > +"[0-9]"s) ]
 				    <[]{ return std::stod(std::string{*m}); } ];
 
-	extern rule Expr;
-
-	rule Value	= n%NUMBER                   <[]{ return *n; }
-				| i%ID > !"="s               <[]{ return v[*i]; }
-				| "(" > e%Expr > ")"         <[]{ return *e; };
-
+	rule Value	= n%NUMBER               <[]{ return *n; }
+				| i%ID > !"="s           <[]{ return v[*i]; }
+				| "(" > e%Expr > ")"     <[]{ return *e; };
 	rule Prod	= l%Value > *(
-				      "*" > r%Value          <[]{ *l *= *r; }
-				    | "/" > r%Value          <[]{ *l /= *r; }
-				)                            <[]{ return *l; };
-
+				      "*" > r%Value      <[]{ *l *= *r; }
+				    | "/" > r%Value      <[]{ *l /= *r; }
+				)                        <[]{ return *l; };
 	rule Sum	= l%Prod > *(
-				      "+" > r%Prod           <[]{ *l += *r; }
-				    | "-" > r%Prod           <[]{ *l -= *r; }
-				)                            <[]{ return *l; };
-
-	rule Expr	= i%ID > "=" > s%Sum         <[]{ return v[*i] = *s; }
-				| s%Sum                      <[]{ return *s; };
-	
-	rule Stmt	= (   "quit"                 <[]{ std::exit(EXIT_SUCCESS); }
-				    | e%Expr                 <[]{ std::cout << *e << std::endl; }
+				      "+" > r%Prod       <[]{ *l += *r; }
+				    | "-" > r%Prod       <[]{ *l -= *r; }
+				)                        <[]{ return *l; };
+	rule Expr	= i%ID > "=" > s%Sum     <[]{ return v[*i] = *s; }
+				| s%Sum                  <[]{ return *s; };
+	rule Stmt	= (   "quit"             <[]{ std::exit(EXIT_SUCCESS); }
+				    | e%Expr             <[]{ std::cout << *e << std::endl; }
 				) > EOL
-				| *( !EOL > "." ) > EOL      <[]{ std::cerr << "syntax error" << std::endl; };
+				| *( !EOL > "." ) > EOL  <[]{ std::cerr << "syntax error" << std::endl; };
 
 	grammar Grammar = start(Stmt);
 }
